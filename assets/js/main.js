324495d0edabd5d15544a416e86e10737688ca85
Yasha Mostofi <yasha.mostofi@gmail.com>
2016-07-25 11:31:37 -0500

    fixed bg link; added highlight of current section on desktop only

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
+ var topRange      = 200,  // 200 measure from the top of the viewport to X pixels down
+     edgeMargin    = 20,   // 20 margin above the top or margin from the end of the page
+     animationTime = 1200, // time in milliseconds
+     contentTop = [];
+
 $(document).ready(function(){
+   // Stop animated scroll if the user does something
+  $('html,body').bind('scroll mousedown DOMMouseScroll mousewheel keyup', function(e){
+    if ( e.which > 0 || e.type == 'mousedown' || e.type == 'mousewheel' ){
+      $('html,body').stop();
+    }
+  });
+
+   // Set up content an array of locations
+  $('.desktop .menu').find('a').each(function(){
+    contentTop.push( $( $(this).attr('href') ).offset().top );
+  });
+
   // Code from W3Schools
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -21,6 +38,19 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     } // End if
+  });
+
+
+   // adjust side menu
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop(),
+    bodyHt = $(document).height(),
+    vpHt = $(window).height() + edgeMargin;  // viewport height + margin
+    $.each( contentTop, function(i,loc){
+      if ( ( loc > winTop - edgeMargin && ( loc < winTop + topRange || ( winTop + vpHt ) >= bodyHt ) ) ){
+        $('.menu a').removeClass('current').eq(i).addClass('current');
+      }
+    })
   });
 
   $('button').click(function() {
