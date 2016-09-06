ee8ec17a16c10afaee616bdfde35ef99dc428937
Simon Lu <lusimon95@gmail.com>
2016-09-06 02:58:59 -0500

    site prototype done

--- mechmania/assets/js/main.js
+++ mechmania/assets/js/main.js
@@ -1,54 +1,87 @@
-var images = [
-    "./assets/img/svg/rp1.svg",
-    "./assets/img/svg/rp2.svg",
-    "./assets/img/svg/rp3.svg",
-    "./assets/img/svg/rp4.svg",
-    "./assets/img/svg/rp5.svg",
-    "./assets/img/svg/rp6.svg",
-    "./assets/img/svg/rp7.svg",
-    "./assets/img/svg/rp8.svg",
-    "./assets/img/svg/rp9.svg",
-    "./assets/img/svg/rp10.svg",
-    "./assets/img/svg/rp11.svg"
-];
 
-$(document).ready(function() {
+console.log("Ready");
+// init controller
+var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
 
-   $(window).scroll(function() {
-      var curr_height = $(window).scrollTop();
-      var total_height = $(window).height();
-      var frame_breakpoint = total_height/11;
+var duration = 1000;
 
-      var custom_floor = function(value, roundTo) {
-         return Math.floor(value / roundTo) * roundTo;
-      };
+// build scenes
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-1", duration: duration})
+    .addTo(controller)
 
-      var curr_idx = Math.floor(custom_floor(curr_height, frame_breakpoint)/(frame_breakpoint*2));
-       
-      if (curr_idx < 12) {
-          $('#frame-img').attr("src", images[curr_idx]);
-      }
-   });
-});
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp1.svg");
+    });
 
 
-//
-// var obj = {curImg: 0};
-//
-// var tween = TweenMax.to(obj, 2,
-//     {
-//         curImg: images.length - 1,
-//         roundProps: "curImg",
-//         repeat: 0,
-//         immediateRender: true,
-//         ease: Linear.easeNone,
-//         onUpdate: function() {
-//             $('#frame-img').attr("src", images[obj.curImg]);
-//         }
-//     });
-//
-// var controller = new ScrollMagic.Controller();
-//
-// var scene = new ScrollMagic.Scene({
-//     triggerElement: '#trigger', duration: 300
-// }).addTo(controller);
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-2", duration: duration})
+    .addTo(controller)
+
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp2.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-3", duration: duration})
+    .addTo(controller)
+
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp3.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-4", duration: duration})
+    .addTo(controller)
+
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp4.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-5", duration: duration})
+    .addTo(controller)
+
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp5.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-6", duration: duration})
+    .addTo(controller)
+
+    .on("enter leave", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp6.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-7", duration: duration})
+    .addTo(controller)
+
+    .on("enter", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp7.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-8", duration: 600})
+    .addTo(controller)
+
+    .on("enter", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp8.svg");
+    });
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-9", duration: 600})
+    .addTo(controller)
+
+    .on("enter", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp9.svg");
+    });
+
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-10", duration: 600})
+    .addTo(controller)
+
+    .on("enter", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp10.svg");
+    });
+
+
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger-11", duration: 600})
+    .addTo(controller)
+
+    .on("enter", function (e) {
+        $("#frame-content").attr("src","./assets/img/svg/rp11.svg");
+    });
