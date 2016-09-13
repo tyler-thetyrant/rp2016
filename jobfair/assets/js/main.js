5e23dfe570c5f13652dd48222d31435ba91f366a
Sujay Khandekar <khandek2@illinois.edu>
2016-09-13 02:07:51 -0500

    switched jobfair code to Handlebars

--- jobfair/assets/js/main.js
+++ jobfair/assets/js/main.js
@@ -1,19 +1,8 @@
-$( document ).ready(function() {
+$(function() {
+    var source = $("#companies-template").html();
+    var template = Handlebars.compile(source);
     $.getJSON('assets/js/companies.json', function (data) {
-        for (var index in data) {
-            var company = data[index];
-            var $mainPane = $("<div>", {"class": "row company-card"});
-            $mainPane.append("<img src='" + company.image +  "' class='col-sm-12 col-lg-3'/>");
-            var textDiv = $("<div>", {"class": "col-sm-12 col-lg-9"})
-            textDiv.append($("<p></p>").text(company.description));
-            textDiv.append($("<p></p>")
-                            .append($("<span style='font-weight:bold'>Opportunities: </span>"))
-                            .append($("<span></span>").text(company.seeking)));
-            textDiv.append($("<p></p>")
-                            .append($("<span style='font-weight:bold'>Work Authorization: </span>"))
-                            .append($("<span></span>").text(company.work)));
-            $mainPane.append(textDiv);
-            $(".companies-list").append($mainPane);
-        }
+        var html = template(data);
+        $('#companies-list').html(html);
     });
-});
+})
